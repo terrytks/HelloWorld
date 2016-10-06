8c91db358637e84aa119469f5f9eb7a378796131
Terry <terry.tchounkongsam@gmail.com>
2016-10-06 11:49:56 +0200

    Premier commit test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello World !");
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("yoyoyo");
+		}
+		
+
+	}
+
+}
